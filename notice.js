e0195d08e14f75ea73a67debedcdd6a8fb98a4ee
Istiyaq123 <57604080+Istiyaq123@users.noreply.github.com>
2020-03-24 10:50:46 +0000

    s5
    h

--- notice.js
+++ notice.js
@@ -1,8 +1,6 @@
 var notice = document.getElementById("info");
-var home = document.getElementById("homeback");
 
 notice.innerHTML = "<strong>&copy2020 Pok&eacute;mon / &copy The Pok&eacute;mon Company / &copy The Pok&eacute;mon Company / &copy DuArt, Film and Video .inc<br>&copy 2020 IstiyaqGames,inc</strong>"
-home.innerHTML = "<a href='\series'>Pok&eacute;mon the Series</a>"
 
 if (screen.width <= 699) {
 	location.replace("https://watchpokemontv.netlify.com/");
