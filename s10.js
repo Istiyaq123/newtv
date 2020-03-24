7278f8b68ea6f81bf16ff7818d2aa0069efcb91f
Istiyaq123 <57604080+Istiyaq123@users.noreply.github.com>
2020-03-24 22:32:02 +0000

    gg

--- s10.js
+++ s10.js
@@ -10,20 +10,20 @@ three.addEventListener("mouseover", wpwc);
 three.addEventListener("mouseout", wpwcO);
 
 function famv() {
-  one.src = "https://pkk.netlify.com/img/s10/1h.png";
+  one.src = "img/s10/1h.png";
 }
 function famvO() {
-  one.src = "https://pkk.netlify.com/img/s10/1.png";
+  one.src = "img/s10/1.png";
 }
 function tdosO() {
-  two.src = "https://pkk.netlify.com/img/s10/2.png";
+  two.src = "img/s10/2.png";
 }
 function tdos() {
-  two.src = "https://pkk.netlify.com/img/s10/2h.png";
+  two.src = "img/s10/2h.png";
 }
 function wpwcO() {
-  three.src = "https://pkk.netlify.com/img/s10/3.png";
+  three.src = "img/s10/3.png";
 }
 function wpwc() {
-  three.src = "https://pkk.netlify.com/img/s10/3h.png";
+  three.src = "img/s10/3h.png";
 }
