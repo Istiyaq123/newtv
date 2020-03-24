e7e58f3be06cf69ad1ec0eaac323a5e65c8771ab
Istiyaq123 <57604080+Istiyaq123@users.noreply.github.com>
2020-03-24 22:38:24 +0000

    ddd

--- s10.js
+++ s10.js
@@ -1,6 +1,7 @@
 var one = document.getElementById("1");
 var two = document.getElementById("2");
 var three = document.getElementById("3");
+var four = document.getElementById("4");
 
 one.addEventListener("mouseover", famv);
 one.addEventListener("mouseout", famvO);
@@ -8,6 +9,8 @@ two.addEventListener("mouseover", tdos);
 two.addEventListener("mouseout", tdosO);
 three.addEventListener("mouseover", wpwc);
 three.addEventListener("mouseout", wpwcO);
+four.addEventListener("mouseover", doane);
+four.addEventListener("mouseout", doaneO);
 
 function famv() {
   one.src = "img/s10/1h.png";
@@ -21,9 +24,10 @@ function tdosO() {
 function tdos() {
   two.src = "img/s10/2h.png";
 }
-function wpwcO() {
-  three.src = "img/s10/3.png";
+function doaneO() {
+  three.src = "img/s10/4.png";
 }
-function wpwc() {
-  three.src = "img/s10/3h.png";
+function doane() {
+  three.src = "img/s10/4h.png";
 }
+
