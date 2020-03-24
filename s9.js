2d905af1d9abc5b6114cd3e7c9a7f3d018835668
Istiyaq123 <57604080+Istiyaq123@users.noreply.github.com>
2020-03-24 16:57:34 +0000

    ss

---
+++ s9.js
@@ -0,0 +1,13 @@
+var 36 = document.getElementById("36");
+
+36.addEventListener("mouseover",mouseOver);
+36.addEventListener("mouseout",mouseOut);
+
+
+function mouseOver() {
+  36.src = "https://pkk.netlify.com/img/s9/36h.png";
+}
+
+function mouseOut() {
+  36.src = "https://pkk.netlify.com/img/s9/36.png";
+}
