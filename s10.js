ef7029734bd1c45fa3209f8d54ff3c72a869ab5d
Istiyaq123 <57604080+Istiyaq123@users.noreply.github.com>
2020-03-25 15:48:20 +0000

    dd
    d

--- s10.js
+++ s10.js
@@ -1,164 +1,166 @@
-var one = document.getElementById("1");
-var two = document.getElementById("2");
-var three = document.getElementById("3");
-var four = document.getElementById("4");
-var five = document.getElementById("5");
-var six = document.getElementById("6");
-var seven = document.getElementById("7");
-var eight = document.getElementById("8");
-var nine = document.getElementById("9");
-var ten = document.getElementById("10");
-var eleven = document.getElementById("11");
-var twelve = document.getElementById("12");
-var thirteen = document.getElementById("13");
-var fourteen = document.getElementById("14");
-var fithteen = document.getElementById("15");
-var sixthteen = document.getElementById("16");
-var seventeen = document.getElementById("17");
-var eighteen = document.getElementById("18");
+function start(){
+  var one = document.getElementById("1");
+  var two = document.getElementById("2");
+  var three = document.getElementById("3");
+  var four = document.getElementById("4");
+  var five = document.getElementById("5");
+  var six = document.getElementById("6");
+  var seven = document.getElementById("7");
+  var eight = document.getElementById("8");
+  var nine = document.getElementById("9");
+  var ten = document.getElementById("10");
+  var eleven = document.getElementById("11");
+  var twelve = document.getElementById("12");
+  var thirteen = document.getElementById("13");
+  var fourteen = document.getElementById("14");
+  var fithteen = document.getElementById("15");
+  var sixthteen = document.getElementById("16");
+  var seventeen = document.getElementById("17");
+  var eighteen = document.getElementById("18");
 
-one.addEventListener("mouseover", famv);
-one.addEventListener("mouseout", famvO);
-two.addEventListener("mouseover", tdos);
-two.addEventListener("mouseout", tdosO);
-three.addEventListener("mouseover", wpwc);
-three.addEventListener("mouseout", wpwcO);
-four.addEventListener("mouseover", doane);
-four.addEventListener("mouseout", doaneO);
-five.addEventListener("mouseover", gtwit);
-five.addEventListener("mouseout", gtwitO);
-six.addEventListener("mouseover", dsfdb);
-six.addEventListener("mouseout", dsfdbO);
-seven.addEventListener("mouseover", lioli);
-seven.addEventListener("mouseout", lioliO);
-eight.addEventListener("mouseover", g);
-eight.addEventListener("mouseout", gO);
-nine.addEventListener("mouseover", stwoib);
-nine.addEventListener("mouseout", stwoibO);
-ten.addEventListener("mouseover", NotOnmyWatchYaDont);
-ten.addEventListener("mouseout", NotOnmyWatchYaDontO);
-eleven.addEventListener("mouseover", MountingACoordinatorAssault);
-eleven.addEventListener("mouseout", MountingACoordinatorAssaultO);
-twelve.addEventListener("mouseover", ArrivalOfARival);
-twelve.addEventListener("mouseout", ArrivalOfARivalO);
-thirteen.addEventListener("mouseover", AStaraviaIsBorn);
-thirteen.addEventListener("mouseout", AStaraviaIsBornO);
-fourteen.addEventListener("mouseover", LeaveItToBrocko);
-fourteen.addEventListener("mouseout", LeaveItToBrockoO);
-fithteen.addEventListener("mouseover", ShapesOfThingsToCome);
-fithteen.addEventListener("mouseout", ShapesOfThingsToComeO);
-sixthteen.addEventListener("mouseover", AGruffActToFollow);
-sixthteen.addEventListener("mouseout", AGruffActToFollowO);
-seventeen.addEventListener("mouseover", WildInTheStreets);
-seventeen.addEventListener("mouseout", WildInTheStreetsO);
-eighteen.addEventListener("mouseover", OErTheRampardosWeWatched);
-eighteen.addEventListener("mouseout", OErTheRampardosWeWatchedO);
+  one.addEventListener("mouseover", famv);
+  one.addEventListener("mouseout", famvO);
+  two.addEventListener("mouseover", tdos);
+  two.addEventListener("mouseout", tdosO);
+  three.addEventListener("mouseover", wpwc);
+  three.addEventListener("mouseout", wpwcO);
+  four.addEventListener("mouseover", doane);
+  four.addEventListener("mouseout", doaneO);
+  five.addEventListener("mouseover", gtwit);
+  five.addEventListener("mouseout", gtwitO);
+  six.addEventListener("mouseover", dsfdb);
+  six.addEventListener("mouseout", dsfdbO);
+  seven.addEventListener("mouseover", lioli);
+  seven.addEventListener("mouseout", lioliO);
+  eight.addEventListener("mouseover", g);
+  eight.addEventListener("mouseout", gO);
+  nine.addEventListener("mouseover", stwoib);
+  nine.addEventListener("mouseout", stwoibO);
+  ten.addEventListener("mouseover", NotOnmyWatchYaDont);
+  ten.addEventListener("mouseout", NotOnmyWatchYaDontO);
+  eleven.addEventListener("mouseover", MountingACoordinatorAssault);
+  eleven.addEventListener("mouseout", MountingACoordinatorAssaultO);
+  twelve.addEventListener("mouseover", ArrivalOfARival);
+  twelve.addEventListener("mouseout", ArrivalOfARivalO);
+  thirteen.addEventListener("mouseover", AStaraviaIsBorn);
+  thirteen.addEventListener("mouseout", AStaraviaIsBornO);
+  fourteen.addEventListener("mouseover", LeaveItToBrocko);
+  fourteen.addEventListener("mouseout", LeaveItToBrockoO);
+  fithteen.addEventListener("mouseover", ShapesOfThingsToCome);
+  fithteen.addEventListener("mouseout", ShapesOfThingsToComeO);
+  sixthteen.addEventListener("mouseover", AGruffActToFollow);
+  sixthteen.addEventListener("mouseout", AGruffActToFollowO);
+  seventeen.addEventListener("mouseover", WildInTheStreets);
+  seventeen.addEventListener("mouseout", WildInTheStreetsO);
+  eighteen.addEventListener("mouseover", OErTheRampardosWeWatched);
+  eighteen.addEventListener("mouseout", OErTheRampardosWeWatchedO);
 
-function famv() {
-  one.src = "img/s10/1h.png";
-}
-function famvO() {
-  one.src = "img/s10/1.png";
-}
-function tdosO() {
-  two.src = "img/s10/2.png";
-}
-function tdos() {
-  two.src = "img/s10/2h.png";
-}
-function wpwc() {
-  three.src = "img/s10/3h.png";
-}
-function wpwcO() {
-  three.src = "img/s10/3.png";
-}
-function doaneO() {
-  four.src = "img/s10/4.png";
-}
-function doane() {
-  four.src = "img/s10/4h.png";
-}
-function gtwitO() {
-  five.src = "img/s10/5.png";
-}
-function gtwit() {
-  five.src = "img/s10/5h.png";
-}
-function dsfdbO() {
-  six.src = "img/s10/6.png";
-}
-function dsfdb() {
-  six.src = "img/s10/6h.png";
-}
-function lioliO() {
-  seven.src = "img/s10/7.png";
-}
-function lioli() {
-  seven.src = "img/s10/7h.png";
-}
-function gO() {
-  eight.src = "img/s10/8.png";
-}
-function g() {
-  eight.src = "img/s10/8h.png";
-}
-function stwoibO() {
-  nine.src = "img/s10/9.png";
-}
-function stwoib() {
-  nine.src = "img/s10/9h.png";
-}
-function NotOnmyWatchYaDontO() {
-  ten.src = "img/s10/10.png";
-}
-function NotOnmyWatchYaDont() {
-  ten.src = "img/s10/10h.png";
-}
-function MountingACoordinatorAssaultO() {
-  eleven.src = "img/s10/11.png";
-}
-function MountingACoordinatorAssault() {
-  eleven.src = "img/s10/11h.png";
-}
-function ArrivalOfARivalO() {
-  twelve.src = "img/s10/12.png";
-}
-function ArrivalOfARival() {
-  twelve.src = "img/s10/12h.png";
-}
-function AStaraviaIsBornO() {
-  thirteen.src = "img/s10/13.png";
-}
-function AStaraviaIsBorn() {
-  thirteen.src = "img/s10/13h.png";
-}
-function LeaveItToBrockoO() {
-  fourteen.src = "img/s10/14.png";
-}
-function LeaveItToBrocko() {
-  fourteen.src = "img/s10/14h.png";
-}
-function ShapesOfThingsToComeO() {
-  fithteen.src = "img/s10/15.png";
-}
-function ShapesOfThingsToCome() {
-  fithteen.src = "img/s10/15h.png";
-}
-function AGruffActToFollowO() {
-  sixthteen.src = "img/s10/16.png";
-}
-function AGruffActToFollow() {
-  sixthteen.src = "img/s10/16h.png";
-}
-function WildInTheStreetsO() {
-  seventeen.src = "img/s10/17.png";
-}
-function WildInTheStreets() {
-  seventeen.src = "img/s10/17h.png";
-}
-function OErTheRampardosWeWatchedO() {
-  eighteen.src = "img/s10/18.png";
-}
-function OErTheRampardosWeWatched() {
-  eighteen.src = "img/s10/18h.png";
+  function famv() {
+    one.src = "img/s10/1h.png";
+  }
+  function famvO() {
+    one.src = "img/s10/1.png";
+  }
+  function tdosO() {
+    two.src = "img/s10/2.png";
+  }
+  function tdos() {
+    two.src = "img/s10/2h.png";
+  }
+  function wpwc() {
+    three.src = "img/s10/3h.png";
+  }
+  function wpwcO() {
+    three.src = "img/s10/3.png";
+  }
+  function doaneO() {
+    four.src = "img/s10/4.png";
+  }
+  function doane() {
+    four.src = "img/s10/4h.png";
+  }
+  function gtwitO() {
+    five.src = "img/s10/5.png";
+  }
+  function gtwit() {
+    five.src = "img/s10/5h.png";
+  }
+  function dsfdbO() {
+    six.src = "img/s10/6.png";
+  }
+  function dsfdb() {
+    six.src = "img/s10/6h.png";
+  }
+  function lioliO() {
+    seven.src = "img/s10/7.png";
+  }
+  function lioli() {
+    seven.src = "img/s10/7h.png";
+  }
+  function gO() {
+    eight.src = "img/s10/8.png";
+  }
+  function g() {
+    eight.src = "img/s10/8h.png";
+  }
+  function stwoibO() {
+    nine.src = "img/s10/9.png";
+  }
+  function stwoib() {
+    nine.src = "img/s10/9h.png";
+  }
+  function NotOnmyWatchYaDontO() {
+    ten.src = "img/s10/10.png";
+  }
+  function NotOnmyWatchYaDont() {
+    ten.src = "img/s10/10h.png";
+  }
+  function MountingACoordinatorAssaultO() {
+    eleven.src = "img/s10/11.png";
+  }
+  function MountingACoordinatorAssault() {
+    eleven.src = "img/s10/11h.png";
+  }
+  function ArrivalOfARivalO() {
+    twelve.src = "img/s10/12.png";
+  }
+  function ArrivalOfARival() {
+    twelve.src = "img/s10/12h.png";
+  }
+  function AStaraviaIsBornO() {
+    thirteen.src = "img/s10/13.png";
+  }
+  function AStaraviaIsBorn() {
+    thirteen.src = "img/s10/13h.png";
+  }
+  function LeaveItToBrockoO() {
+    fourteen.src = "img/s10/14.png";
+  }
+  function LeaveItToBrocko() {
+    fourteen.src = "img/s10/14h.png";
+  }
+  function ShapesOfThingsToComeO() {
+    fithteen.src = "img/s10/15.png";
+  }
+  function ShapesOfThingsToCome() {
+    fithteen.src = "img/s10/15h.png";
+  }
+  function AGruffActToFollowO() {
+    sixthteen.src = "img/s10/16.png";
+  }
+  function AGruffActToFollow() {
+    sixthteen.src = "img/s10/16h.png";
+  }
+  function WildInTheStreetsO() {
+    seventeen.src = "img/s10/17.png";
+  }
+  function WildInTheStreets() {
+    seventeen.src = "img/s10/17h.png";
+  }
+  function OErTheRampardosWeWatchedO() {
+    eighteen.src = "img/s10/18.png";
+  }
+  function OErTheRampardosWeWatched() {
+    eighteen.src = "img/s10/18h.png";
+  }
 }
